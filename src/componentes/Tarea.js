6ac5e8569e56e6bc2dc7132dce4f991c1bfdf577
Andrespampa <andrespampa@gmail.com>
2026-05-19 22:36:13 -0300

    terminando formulario para editar tareas - video 31

--- src/componentes/Tarea.js
+++ src/componentes/Tarea.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckSquare, faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
 const Tarea = ({tarea}) => {
+
+    const [editandoTarea, cambiarEditandoTarea] = useState(false); //Cuando cargue la pagina no estamos editando ninguna tarea
+    const [nuevaTarea, cambiarNuevaTarea] = useState(tarea.texto);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        cambiarEditandoTarea(false);
+    }
+
     return (
         <li className='lista-tareas__tarea'>
             <FontAwesomeIcon 
                 icon={faCheckSquare} 
                 className='lista-tareas__icono lista-tareas__icono-check' />
             <div className='lista-tareas__texto'>
-                {tarea.texto}
+                {
+                    editandoTarea === true ? 
+                        <form action="" className='formulario-editar-tarea' onSubmit={handleSubmit}>
+                            <input type="text" className='formulario-editar-tarea__input' value={nuevaTarea} onChange={(e) => cambiarNuevaTarea(e.target.value)}/>
+                            <button type='submit' className='formulario-editar-tarea__btn'>Editar tarea</button>
+                        </form>
+                        : tarea.texto
+                }
             </div>
             <div className='lista-tareas__contenedor-botones'>
                 <FontAwesomeIcon 
                     icon={faEdit} 
-                    className='lista-tareas__icono lista-tareas__icono-accion' />
+                    className='lista-tareas__icono lista-tareas__icono-accion' 
+                    onClick={() => cambiarEditandoTarea(!editandoTarea)}/>
                 <FontAwesomeIcon 
                     icon={faTimes} 
                     className='lista-tareas__icono lista-tareas__icono-accion' />
